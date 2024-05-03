6966dbb027f7736b6330dabbb1be39b7859f8d20
Tadhg Deeney <cybertadhg@gmail.com>
2024-05-03 23:07:29 +0100

    To-Do List Project
    Adding comments and improvments to for loop/for each functionality.

--- apps_scripts/to_do_list_tracker.js
+++ apps_scripts/to_do_list_tracker.js
@@ -1,60 +1,75 @@
-var SHARE_URL = 'https://docs.google.com/spreadsheets/d/1HABCawhwKAwj6rm1sWmwIqlnndifa4Z20hpPO8eeeFQ/edit?usp=sharing';
-var RECIPIENT_EMAILS = ['tdeeney19@gmail.com']
+/**
+ * author: Tadhg Deeney
+ * date: 2024-05-02
+ * description: 
+ * A handy little tool to help remind me of the tasks I'm putting off 
+ * to change my approach or break them down into manageable tasks & keep track.
+ */
 
+// GLOBAL VARIABLES
+var SHARE_URL = 'GOOGLE_SHEET_LINK';
+var RECIPIENT_EMAILS = ['EMAIL_ADDRESS@gmail.com'];
+
+/**
+ * Function to check the to-domlist & send email notifications
+ * if the tasks are overdue.
+ */
 function check_list() {
   var today_date = new Date();
   var ss = SpreadsheetApp.openByUrl(SHARE_URL);
   var sheet = ss.getSheetByName('To-Do List');
-  var sheet_value = sheet.getDataRange().getValues();
+  var sheet_values = sheet.getDataRange().getValues();
   var output = [];
-  for(var i = 3; i < sheet_value.length; i++){
-    var due_date = new Date(sheet_value[i][0]);
-    var time_difference = today_date.getTime() - due_date.getTime();  
-    var days_overdue = parseInt(time_difference / (1000 * 60 * 60 * 24));  
-    Logger.log("%s - %s = %s", due_date, today_date, days_overdue);
-    if(days_overdue > 0 && sheet_value[i][1] != true){
-      var date_formatted = Utilities.formatDate(due_date, 'Dublin/Europe', "YYYY-MM-dd");
-      output.push([sheet_value[i][2], date_formatted, days_overdue]);
+  
+  // Loop through each row in the sheet, starting from index 3 to only get tasks added to the list & not the headers.
+  sheet_values.slice(3).forEach(function(row) {
+    var due_date = new Date(row[0]);
+    var time_difference = today_date.getTime() - due_date.getTime();
+    var days_overdue = Math.max(0, Math.floor(time_difference / (1000 * 60 * 60 * 24)));
+    // If the task is overdue and not marked as complete:
+    if (days_overdue > 0 && !row[1]) {
+      var date_formatted = Utilities.formatDate(due_date, Session.getScriptTimeZone(), 'yyyy-MM-dd');
+      output.push([row[2], date_formatted, days_overdue]);
     }
-  }
-  Logger.log("%s - %s", output, output.length);
-  if (output.length >= 1){
+  });
+  // If there are overdue tasks, send the email notifications.
+  if (output.length >= 1) {
     send_email(output);
-  }
-  else{
-    Logger.log("No emails to send today!");
+  } else {
+    Logger.log('No emails to send today!');
   }
 }
 
-
+/**
+ * A Function to send email notifications of the overdue tasks.
+ * @param {array} results - a 2D array of all of the overdue tasks
+ */
 function send_email(results) {
-    var date = new Date();
-    date = Utilities.formatDate(date, 'Dublin/Europe', "YYYY-MM-dd");
-    var subject = 'Keep Track - ' +date;
-    var html_body = '<html><body>' +
-        '<p>Hi Tadhg,</p>' +
-        '<p>' +
-        'See full list <a href="'+SHARE_URL+'">here</a>.<br/>';
-        html_body += '<table border="1" width="95%" style="border-collapse:collapse;">';
-        html_body += '<tr>';
-        html_body += '<td align="left"><b>Task</b></td>';
-        html_body += '<td align="center"><b>Due Date</b></td>';
-        html_body += '<td align="center"><b>Overdue</b></td>';
-        html_body += '</tr>';
-        
-        for (var i in results) {
-            var row = results[i];
-            Logger.log(row);
-            html_body += '<tr><td align="left">' + row[0] +
-            '</td><td align="center">' + row[1] +  
-            '</td><td align="center">' + row[2] +
-            '</td></tr>';
-        }
-        html_body += '</table>';
-        html_body += '<br/><p>Kind Regards, <br/>The Habit Tracker</p></body></html>';
-    var body = 'Please enable HTML to view this email.';
-    var options = {htmlBody: html_body};
-    for(var j in RECIPIENT_EMAILS){
-      MailApp.sendEmail(RECIPIENT_EMAILS[j], subject, body, options);
-    }
+  var date = Utilities.formatDate(new Date(), Session.getScriptTimeZone(), 'yyyy-MM-dd');
+  var subject = 'Keep Track - ' + date;
+
+  var html_body = '<html><body>' +
+    '<p>Hi Tadhg,</p>' +
+    '<p>See full list <a href="' + SHARE_URL + '">here</a>.</p>' +
+    '<table border="1" width="95%" style="border-collapse:collapse;">' +
+    '<tr>' +
+    '<td align="left"><b>Task</b></td>' +
+    '<td align="center"><b>Due Date</b></td>' +
+    '<td align="center"><b>Overdue</b></td>' +
+    '</tr>';
+  // Add each task  details to the table of the email.
+  results.forEach(function(row) {
+    html_body += '<tr><td align="left">' + row[0] +
+      '</td><td align="center">' + row[1] +
+      '</td><td align="center">' + row[2] +
+      '</td></tr>';
+  });
+
+  html_body += '</table>' +
+    '<br/><p>Kind Regards,<br/>The Habit Tracker</p></body></html>';
+  // Send the email notification to each recipient
+  var options = { htmlBody: html_body };
+  RECIPIENT_EMAILS.forEach(function(email) {
+    MailApp.sendEmail(email, subject, 'Please enable HTML to view this email.', options);
+  });
 }
