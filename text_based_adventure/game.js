0c8324e973f36bc74e7f3a3f04970826fc8c33c8
Tadhg Deeney <cybertadhg@gmail.com>
2024-08-26 23:01:05 +0100

    Added the Score & Inspiration
    Introduced a Bug in the CSS for the Choice Buttons, they buttons are no longer responding to being hidden from view when disabled.

--- text_based_adventure/game.js
+++ text_based_adventure/game.js
@@ -1,3 +1,9 @@
+let choices;
+let playerStats = {
+    score: 0, // Initialise the score to 0
+    inspiration: false
+};
+
 async function loadStory() {
     try {
       let response = await fetch('story.json');
@@ -9,20 +15,20 @@ async function loadStory() {
     } catch (error) {
       console.error('Failed to load story:', error);
     }
-  }
+}
   
-  async function loadChapter(story, chapterNumber) {
+async function loadChapter(story, chapterNumber) {
     let chapterKey = `chapter_${chapterNumber}`;
     console.log(chapterKey);
     let chapter = story[chapterKey];
     if (!chapter) {
-      console.error('Chapter not found:', chapterNumber);
-      return null;
+        console.error('Chapter not found:', chapterNumber);
+        return null;
     }
     return chapter;
-  }
+}
 
-  function typeWriter(text, element, callback) {
+function typeWriter(text, element, callback) {
     let i = 0;
     const speed = 25; // Speed in milliseconds
 
@@ -56,82 +62,87 @@ async function loadStory() {
     type();
 }
 
-
-
 function handleChoice(choiceNumber){
     const choice = choices[choiceNumber];
-    if (choice) {
-        // Disable buttons during typewriter effect
-        const successButton = document.getElementById('succession');
-        const failureButton = document.getElementById('failure');
-        successButton.disabled = true;
-        failureButton.disabled = true;
 
-        // Update the description with typewriter effect
-        const descriptionElement = document.querySelector(".prompt-description");
-        descriptionElement.innerHTML = ''; // Clear the element's content before starting
-        successButton.style.display = 'none';
-        failureButton.style.display = 'none';
-
-        // Check for inspiration
-        const inspirationElement = document.querySelector(".inspiration");
-        if (choice.inspiration) {
-            inspirationElement.style.color = '#4CAF50';
-            inspirationElement.style.pointerEvents = 'auto'; // Make it clickable
-            inspirationElement.classList.add('green');
-            inspirationElement.onclick = () => {
-                inspirationElement.style.color = 'grey';
-                inspirationElement.style.pointerEvents = 'none'; // Make it unclickable again
-                // Additional logic when inspiration is clicked
-                inspirationElement.classList.remove('green');
-                handleInspirationClick();
-            };
-        } /*else {
-            inspirationElement.style.color = 'grey';
-            inspirationElement.style.pointerEvents = 'none'; // Make it unclickable
-            inspirationElement.classList.remove('green');
-        }*/
-
-        var amount = document.querySelector(".coin");
-        if (choice.coin){
-            playerStats.score += amount;
-            document.getElementById('score-value').innerText = playerStats.score;
-        }
-        
-        // console.log(choice.description);
-        typeWriter(choice.description, descriptionElement, () => {
-           
-            successButton.disabled = false;
-            failureButton.disabled = false;
-
-            // Update the buttons with the next choices
-            successButton.innerText = choice.success.text;
-            successButton.style.display = 'inline-block'; // Ensure the button is visible
-            successButton.onclick = () => handleChoice(choice.success.next);
-
-            if (choice.failure) {
-                failureButton.innerText = choice.failure.text;
-                failureButton.style.display = 'inline-block'; // Ensure the button is visible
-                failureButton.onclick = () => handleChoice(choice.failure.next);
-            } else {
-                failureButton.style.display = 'none'; // Hide the button if no failure option
-            }
-        });
-    } else {
-        console.log('No more choices available.');
+    if (!choice) {
+        console.log('No more choices available...');
+        return;
     }
-    //updateStats();
+
+    disableButtons();
+    clearDescription();
+    handleInspiration(choice);
+    updateScore(choice);
+    typeWriter(choice.description, document.querySelector(".prompt-description"), () => {
+        setTimeout(() => {
+            enableButtons();
+            updateButtons(choice);
+        }, 500);
+    });
 }
 
-let choices;
-var playerStats = {
-    score: 0
-};
+function disableButtons(){
+    console.log('Disabling buttons');
+    document.getElementById('succession').disabled = true;
+    document.getElementById('failure').disabled = true;
+}
 
-function handleInspirationClick() {
-    // Add your logic for what happens when the inspiration element is clicked
-    console.log('Gain Inspiration.');
-    // For example, you could add logic to increase a player's inspiration points
+function enableButtons(){
+    console.log('Enabling buttons');
+    document.getElementById('succession').disabled = false;
+    document.getElementById('failure').disabled = false;
+}
+
+function updateButtons(choice) {
+    const successButton = document.getElementById('succession');
+    const failureButton = document.getElementById('failure');
+
+    successButton.innerText = choice.success.text;
+    successButton.style.display = 'inline-block';
+    successButton.onclick = () => handleChoice(choice.success.next);
+
+    if (choice.failure) {
+        failureButton.innerText = choice.failure.text;
+        failureButton.style.display = 'inline-block';
+        failureButton.onclick = () => handleChoice(choice.failure.next);
+    } else {
+        failureButton.style.display = 'none';
+    }
+}
+
+function clearDescription() {
+    const descriptionElement = document.querySelector(".prompt-description");
+    descriptionElement.innerHTML = ''; // Clear the element's content
+}
+
+function handleInspiration(choice) {
+    const inspirationElement = document.querySelector(".inspiration");
+
+    if (choice.inspiration) {
+        playerStats.inspiration = true;
+    }
+
+    if (playerStats.inspiration) {
+        inspirationElement.style.color = '#4CAF50';
+        inspirationElement.style.pointerEvents = 'auto';
+        inspirationElement.classList.add('green');
+
+        inspirationElement.onclick = () => {
+            inspirationElement.style.color = 'grey';
+            inspirationElement.style.pointerEvents = 'none';
+            inspirationElement.classList.remove('green');
+
+            playerStats.inspiration = false;
+        };
+    } 
+}
+
+function updateScore(choice) {
+    if (choice.coin) {
+        playerStats.score += choice.coin;
+        document.getElementById('score-value').innerText = playerStats.score;
+    }
 }
 
 document.getElementById('start-btn').addEventListener('click', () => {
